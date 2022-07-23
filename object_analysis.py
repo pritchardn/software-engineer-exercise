8ba934660c3543784a4b8ce30e4b36febd0f31d5
Nicholas Pritchard <30886786+pritchardn@users.noreply.github.com>
2022-07-23 12:11:07 +0800

    9 - Adds docstrings for functions and modules, formats code consistently.

--- object_analysis.py
+++ object_analysis.py
@@ -1,35 +1,41 @@
+"""
+Contains function to calculate basic stats (frequency and max) on a list of astronomical
+object dictionaries
+"""
+
 import json
 from collections import defaultdict
 
 
-def most_common_type(objects: list) -> str:
+def most_common_type(object_list: list) -> str:
+    """
+    Calculates the 'type' attribute with the highest frequency in a list of dictionaries
+    :param object_list: A list of dictionary objects. All are assumed to have a 'type' key
+    :return: The first, most frequent 'type' in the list of objects.
+    """
     type_counts = defaultdict(int)
-    for o in objects:
-        type_counts[o['type']] += 1
+    for entry in object_list:
+        type_counts[entry["type"]] += 1
     return max(type_counts, key=lambda x: type_counts[x])
 
 
 def farthest(objects: list) -> dict:
+    """
+    Finds the object with the largest 'redshift' value in a list of objects.
+    :param objects: A list of objects. All are assumed to contain a 'redshift' field.
+    :return: The first object with the largest redshift value.
+    """
     return max(objects, key=lambda x: x["redshift"])
 
 
 def main():
+    """
+    A basic example script showing the farthest and abundant functions on a basic test-case.
+    """
     example_input = [
-        {
-            "type": "star",
-            "name": "alpha-centaurus",
-            "redshift": 0
-        },
-        {
-            "type": "nebula",
-            "name": "crab",
-            "redshift": 0
-        },
-        {
-            "type": "galaxy",
-            "name": "sombrero",
-            "redshift": 0
-        }
+        {"type": "star", "name": "alpha-centaurus", "redshift": 0},
+        {"type": "nebula", "name": "crab", "redshift": 0},
+        {"type": "galaxy", "name": "sombrero", "redshift": 0},
     ]
     print(f"Example input is:\n{json.dumps(example_input,indent=4)}")
     print(f"Most abundant is {most_common_type(example_input)}")
@@ -38,4 +44,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
--- tests/test_object_analysis.py
+++ tests/test_object_analysis.py
@@ -1,9 +1,17 @@
+"""
+Tests the functions in object_analysis.py for correctness.
+"""
+
 from unittest import TestCase
 
 from object_analysis import most_common_type, farthest
 
 
 class TestFarthest(TestCase):
+    """
+    Tests the farthest function for correctness.
+    """
+
     def test_farthest(self):
         """
         Asserts that farthest returns the object with the single maximum redshift value
@@ -11,40 +19,30 @@ class TestFarthest(TestCase):
         maximum_redshift_object = {
             "type": "star",
             "name": "alpha-centaurus",
-            "redshift": 100
+            "redshift": 100,
         }
         test_data = [
             maximum_redshift_object,
-            {
-                "type": "nebula",
-                "name": "crab",
-                "redshift": 5
-            },
-            {
-                "type": "galaxy",
-                "name": "sombrero",
-                "redshift": 0
-            }
+            {"type": "nebula", "name": "crab", "redshift": 5},
+            {"type": "galaxy", "name": "sombrero", "redshift": 0},
         ]
         calculated_output = farthest(test_data)
         self.assertEqual(maximum_redshift_object, calculated_output)
 
 
 class TestMostCommonType(TestCase):
+    """
+    Tests the abundant function for correctness
+    """
+
     def test_max_frbs(self):
         """
         Asserts that a list of objects mostly containing frbs will return 'frbs'
         """
         test_data = [
-            {
-                "type": "star",
-            },
-            {
-                "type": "frb",
-            },
-            {
-                "type": "frb",
-            }
+            {"type": "star"},
+            {"type": "frb"},
+            {"type": "frb"},
         ]
         output_calculated = most_common_type(test_data)
         self.assertEqual("frb", output_calculated)
@@ -54,15 +52,9 @@ class TestMostCommonType(TestCase):
         Asserts that a list of objects mostly containing nebulae will return 'nebulae
         """
         test_data = [
-            {
-                "type": "nebula",
-            },
-            {
-                "type": "nebula",
-            },
-            {
-                "type": "frb",
-            }
+            {"type": "nebula"},
+            {"type": "nebula"},
+            {"type": "frb"},
         ]
         output_calculated = most_common_type(test_data)
         self.assertEqual("nebula", output_calculated)
