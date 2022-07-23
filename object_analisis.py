0ae9eec94141d93d0eccf3eaf9c82d5116a09860
Nicholas Pritchard <30886786+pritchardn@users.noreply.github.com>
2022-07-23 11:42:56 +0800

    3 - aboundant now correctly calculates most abundant object type. Farthest correctly calculates object with largest redshift value.

--- object_analisis.py
+++ object_analisis.py
@@ -3,6 +3,7 @@ def aboundant(objects):
     sum_galaxies = 0
     sum_supernovae = 0
     sum_frbs = 0
+    sum_nebulae = 0
     for o in objects:
         if o['type'] == 'star':
             sum_stars += 1
@@ -14,15 +15,20 @@ def aboundant(objects):
             sum_supernovae += 1
     for o in objects:
         if o['type'] == 'frb':
-            sum_supernovae += 1
-    if sum_stars >= sum_galaxies and sum_stars >= sum_supernovae and sum_stars >= sum_frbs:
+            sum_frbs += 1
+    for o in objects:
+        if o['type'] == 'nebula':
+            sum_nebulae += 1
+    if sum_stars >= sum_galaxies and sum_stars >= sum_supernovae and sum_stars >= sum_frbs and sum_stars >= sum_nebulae:
         return 'stars'
-    if sum_galaxies >= sum_stars and sum_galaxies >= sum_supernovae and sum_galaxies >= sum_frbs:
+    if sum_galaxies >= sum_stars and sum_galaxies >= sum_supernovae and sum_galaxies >= sum_frbs and sum_galaxies >= sum_nebulae:
         return 'galaxies'
-    if sum_supernovae >= sum_stars and sum_supernovae >= sum_galaxies and sum_supernovae >= sum_frbs:
+    if sum_supernovae >= sum_stars and sum_supernovae >= sum_galaxies and sum_supernovae >= sum_frbs and sum_supernovae >= sum_nebulae:
         return 'supernovae'
-    if sum_frbs >= sum_stars and sum_frbs >= sum_galaxies and sum_frbs >= sum_supernovae:
+    if sum_frbs >= sum_stars and sum_frbs >= sum_galaxies and sum_frbs >= sum_supernovae and sum_frbs >= sum_nebulae:
         return 'frbs'
+    if sum_nebulae >= sum_stars and sum_nebulae >= sum_galaxies and sum_nebulae >= sum_supernovae and sum_nebulae >= sum_frbs:
+        return 'nebulae'
 
 input = """
 [
@@ -51,7 +57,7 @@ print(aboundant(json.loads(input)))
 def farthest(objects):
     highest_redshift = None
     for o in objects:
-        if highest_redshift is None or o["redshift"] < highest_redshift:
+        if highest_redshift is None or o["redshift"] > highest_redshift:
             highest_redshift = o["redshift"]
     for o in objects:
         if o["redshift"] == highest_redshift:
