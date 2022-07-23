bd8c59fd533381cd1b90034bea5ce70f10683ebf
Nicholas Pritchard <30886786+pritchardn@users.noreply.github.com>
2022-07-23 12:03:56 +0800

    8 - Refactors default script code to main-function style.

--- object_analisis.py
+++ object_analisis.py
@@ -1,3 +1,4 @@
+import json
 from collections import defaultdict
 
 
@@ -8,33 +9,33 @@ def most_common_type(objects: list) -> str:
     return max(type_counts, key=lambda x: type_counts[x])
 
 
-input = """
-[
-    {
-        "type": "star",
-        "name": "alpha-centaurus",
-        "redshift": 0
-    },
-    {
-        "type": "nebula",
-        "name": "crab",
-        "redshift": 0
-    },
-    {
-        "type": "galaxy",
-        "name": "sombrero",
-        "redshift": 0
-    }
-]
-"""
-
-import json
-
-print(most_common_type(json.loads(input)))
-
-
 def farthest(objects: list) -> dict:
     return max(objects, key=lambda x: x["redshift"])
 
 
-print(farthest(json.loads(input)))
+def main():
+    example_input = [
+        {
+            "type": "star",
+            "name": "alpha-centaurus",
+            "redshift": 0
+        },
+        {
+            "type": "nebula",
+            "name": "crab",
+            "redshift": 0
+        },
+        {
+            "type": "galaxy",
+            "name": "sombrero",
+            "redshift": 0
+        }
+    ]
+    print(f"Example input is:\n{json.dumps(example_input,indent=4)}")
+    print(f"Most abundant is {most_common_type(example_input)}")
+    print(f"Farthest is {farthest(example_input)}")
+
+
+if __name__ == "__main__":
+    main()
+
