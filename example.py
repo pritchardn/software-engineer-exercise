cd4892118255f7f1c63352b92279b48b0f81cf9a
Nicholas Pritchard <30886786+pritchardn@users.noreply.github.com>
2022-07-23 12:45:44 +0800

    8c - Ever so slightly more interesting example

--- example.py
+++ example.py
@@ -9,7 +9,7 @@ def main():
     example_input = [
         {"type": "star", "name": "alpha-centaurus", "redshift": 0},
         {"type": "nebula", "name": "crab", "redshift": 0},
-        {"type": "galaxy", "name": "sombrero", "redshift": 0},
+        {"type": "galaxy", "name": "sombrero", "redshift": 10},
     ]
     print(f"Example input is:\n{json.dumps(example_input,indent=4)}")
     print(f"Most abundant is {most_common_type(example_input)}")
