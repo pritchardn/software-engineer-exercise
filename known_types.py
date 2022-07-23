ea4f53e536a6bbc566fbd3e099ac701e8c077ca0
Nicholas Pritchard <30886786+pritchardn@users.noreply.github.com>
2022-07-23 13:58:24 +0800

    Elaborates on docstring in known_types.py

--- known_types.py
+++ known_types.py
@@ -1,5 +1,7 @@
 """
 Contains a definition of expected object types and their plural forms.
+Defining this separately means editing this object changes the behaviour of the function
+and the unittests simultaneously.
 """
 
 known_types_plurals = {
